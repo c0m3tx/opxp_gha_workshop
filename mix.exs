da3995a1a637f39afabafadec3bea784f037c8f9
dinopascale-prima <dino.pascale@prima.it>
2024-01-22 15:29:06 +0100

    add some fake dependencies

--- mix.exs
+++ mix.exs
@@ -22,6 +22,12 @@ defmodule GhaWorkshop.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:jason, "~> 1.2"},
+      {:joken, "~> 2.6.0"},
+      {:money, "~> 1.4"},
+      {:poison, "~> 5.0.0"},
+      {:telepoison, "~> 1.3"},
+      {:timex, "~> 3.6"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
